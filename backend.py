03c077c77adfe8b5166a310c0336504fdfff5743
HeenaDarji <161707142+HeenaDarji@users.noreply.github.com>
2026-01-10 03:38:21 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import csv
 from datetime import datetime
 from ultralytics import YOLO
 import os
-MODEL_PATH = os.path.join(BASE_DIR, "best.pt")
+MODEL_PATH ="best.pt"
 
 model = YOLO(MODEL_PATH)
 
